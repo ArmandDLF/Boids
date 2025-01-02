04a95c1e17d581ecb7516792cf6e5ae8d7c79dcf
ArmandDLF <armand.de_fontenay@etu.minesparis.psl.eu>
2025-01-02 12:14:13 +0100

    Moving boids towards barycenter, Add boids on click

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ struct global_t {
 
 	// Objects
 	std::vector<Object*> sceneObjects;
+	int N = 0; // Number of boids
+	float bary_x; // Barycenter of boids
+	float bary_y;
 
 	// random
 	std::random_device rd;
@@ -30,26 +33,26 @@ global_t w;
 
 class Object {
 	protected:
-		float x;
-		float y;
 		int scale;
 		std::tuple<Uint8, Uint8, Uint8, Uint8> color;
 	public:
-		Object (float x, float y, int scale, std::tuple<Uint8, Uint8, Uint8, Uint8> color) 
-			: x(x), y(y), scale(scale), color(color) {}
+		bool obstacle;
+		float x;
+		float y;
+		Object (float x, float y, int scale, std::tuple<Uint8, Uint8, Uint8, Uint8> color, bool obstacle) 
+			: x(x), y(y), scale(scale), color(color), obstacle(obstacle) {}
 		virtual int draw() = 0;
-		virtual int update() = 0;
+		virtual void update() = 0;
 };
 
 class Boids : public Object {
 	private:
-		float direction; // [0,2*pi[
-		// x,y coordinates that are inherited represent the middle of the base line
+		float vx; // Velocity in x direction
+    	float vy; // Velocity in y direction
+		// x,y coordinates that are inherited represent the middle of the base line of the triangle
 	public:
-		Boids (float x, float y, int scale, std::tuple<Uint8, Uint8, Uint8, Uint8> color, float direction) 
-		: Object(x,y,scale,color) {
-			this->direction = direction;
-		}
+		Boids (float x, float y, int scale, std::tuple<Uint8, Uint8, Uint8, Uint8> color, float vx, float vy) 
+		: Object(x,y,scale,color, false), vx(vx), vy(vy) {}
 
 		int draw() override {
 			// Draws the boid to the screen
@@ -57,13 +60,14 @@ class Boids : public Object {
 			Uint8 r, g, b, a;
 			std::tie(r,g,b,a) = color;
 
+			float direction = std::atan2(vy, vx);
 			float orthogonal = direction + M_PI / 2;
 			if (orthogonal > 2*M_PI) orthogonal -= 2*M_PI;
 
-			float delta_x = scale / 2 * std::sinf(orthogonal); // l * sin(PI - theta')
-			float delta_y = - scale / 2 * std::cosf(orthogonal); // l * cos(PI - theta')
-			float delta_x_normal = 1.5*scale*std::sinf(direction);
-			float delta_y_normal = -1.5*scale*std::cosf(direction);
+			float delta_x = scale / 2 * std::cosf(orthogonal); // l * cos(orthogonal)
+       		float delta_y = scale / 2 * std::sinf(orthogonal); // l * sin(orthogonal)
+			float delta_x_normal = 1.5 * scale * std::cosf(direction);
+			float delta_y_normal = 1.5 * scale * std::sinf(direction);
 
 			const std::vector< SDL_Vertex > verts =
 				{
@@ -75,25 +79,70 @@ class Boids : public Object {
 			return SDL_RenderGeometry(w.renderer, nullptr, verts.data(), verts.size(), nullptr, 0);
 		}
 
-		int update() override {
-			// Updates the boid
-			return 0;
+		void turn_towards(float dir_x, float dir_y) {
+			// Turns the boid progressively towards a direction
+			const float RAD_STEP = 0.05;
+
+			float target = std::atan2(dir_y, dir_x);
+			float direction = std::atan2(vy, vx);
+			float diff = target - direction;
+			if (fabs(diff) < RAD_STEP) return; // If close enough, it will stop jittering
+
+			if (diff > M_PI) diff -= 2*M_PI;
+			if (diff < -M_PI) diff += 2*M_PI;
+			if (diff > 0) {
+				direction += RAD_STEP;
+			} else {
+				direction -= RAD_STEP;
+			}
+
+			float norm = std::sqrt(vx*vx + vy*vy);
+			vx = std::cos(direction)*norm;
+			vy = std::sin(direction)*norm;
 		}
+		void update() override {
+			// Turn towards the barycenter
+			if (w.N > 1){ 
+				// If there is only 1 boid, it does not make sense to turn towards the barycenter
+				turn_towards(w.bary_x - x, w.bary_y - y);
+			}
+
+			// Updates the boid's position based on its velocity
+        	x += vx;
+        	y += vy;
+
+			}
 };
 
 void do_render() {
 	// Reset to black screen
 	SDL_SetRenderDrawColor(w.renderer, 0u, 0u, 0u, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(w.renderer);
-
+	
+	// Render the scene
 	for (Object* x : w.sceneObjects){
 		x->draw();
 	}
+
+	// Draw point at barycenter
+	SDL_SetRenderDrawColor(w.renderer, 0u, 255u, 0u, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawPoint(w.renderer, w.bary_x, w.bary_y);
 
 	SDL_RenderPresent(w.renderer);
 }
 
 void do_update() {
+	// Update the barycenter
+	float sum_x = 0;
+	float sum_y = 0;
+	for (Object* obj : w.sceneObjects){
+		if (obj->obstacle) continue;
+		sum_x += obj->x;
+		sum_y += obj->y;
+	}
+	w.bary_x = sum_x / w.N;
+	w.bary_y = sum_y / w.N;	
+
 	// Update the scene
 	for (Object* x : w.sceneObjects){
 		x->update();
@@ -101,9 +150,9 @@ void do_update() {
 }
 
 void init_scene(){
-	auto color = std::make_tuple<Uint8, Uint8, Uint8, Uint8>(255,0,0,255);
-	Boids * b1 =  new Boids(WIDTH/2, HEIGHT/2, 25, color, 0.5);
-	w.sceneObjects.push_back(b1);
+	// auto color = std::make_tuple<Uint8, Uint8, Uint8, Uint8>(255,0,0,255);
+	// Boids * b1 =  new Boids(WIDTH/2, HEIGHT/2, 25, color, 1., 0.);
+	// w.sceneObjects.push_back(b1);
 }
 
 int main(int argc, char ** argv)
@@ -150,6 +199,15 @@ int main(int argc, char ** argv)
 				}
 				break;
 			case SDL_KEYUP:
+				break;
+			case SDL_MOUSEBUTTONDOWN:					
+				// Add a boid on left click where the mouse is
+				if (event.button.button == SDL_BUTTON_LEFT){
+					auto color = std::make_tuple<Uint8, Uint8, Uint8, Uint8>(255,0,0,255);
+					Boids * b1 =  new Boids(event.button.x, event.button.y, 25, color, 1., 0.);
+					w.sceneObjects.push_back(b1);
+					w.N++;
+				}
 				break;
 			}
 		} else {
